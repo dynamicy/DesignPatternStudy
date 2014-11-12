7d6e8f86887acb7e9dc2168752d66dfff7a80152
sufrank <su.frank@gmail.com>
2014-11-13 00:27:25 +0800

    add C# Registry of Singleton sample

---
+++ RegistryofSingletonsPattern/CSharp/RegistryOfSingleton.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace DesignPatthenStudy
+{
+    class RegistryOfSingleton
+    {
+        static void Main(string[] args)
+        {
+            SingletonRegistry.Register("RegistryOfSingleton.MySingleton", new MySingleton());
+
+            object tSingleton = SingletonRegistry.Lookup("RegistryOfSingleton.MySingleton");
+            if (null != tSingleton)
+                Console.WriteLine(((DesignPatthenStudy.MySingleton)tSingleton).InstanceValue);
+        }
+    }
+
+    public sealed class MySingleton
+    {
+        public MySingleton() {}
+
+        public string InstanceValue
+        {
+            get { return "This is MySingleton"; }
+        }
+    }
+
+    public class SingletonRegistry
+    {
+        private static Dictionary<string, object> mRegister = new Dictionary<string, object>();
+        private SingletonRegistry() { }
+
+        public static void Register(string pName, object pInstance)
+		{
+            if(!mRegister.ContainsKey(pName))
+                mRegister.Add(pName, pInstance);
+		}
+
+        public static object Lookup(string pName)
+		{
+            if (!mRegister.ContainsKey(pName))
+                return null;
+            else
+                return (object)mRegister[pName];
+		}
+    }
+}
---
+++ RegistryofSingletonsPattern/CSharp/RegistryOfSingletonWithReflection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace DesignPatthenStudy
+{
+    class RegistryOfSingletonWithReflection
+    {
+        static void Main(string[] args)
+        {
+            object tSingleton = SingletonRegistry.Lookup("RegistryOfSingletonWithReflection.MySingleton");
+            if (null != tSingleton)
+                Console.WriteLine(((DesignPatthenStudy.MySingleton)tSingleton).InstanceValue);
+        }
+    }
+
+    public class MySingleton
+    {
+        public string InstanceValue
+        {
+            get { return "This is MySingleton"; }
+        }
+    }
+
+    public class SingletonRegistry
+    {
+        private static Dictionary<string, object> mRegister = new Dictionary<string, object>();
+        private SingletonRegistry() { }
+
+        public static void Register(string pName, object pInstance)
+        {
+            if (!mRegister.ContainsKey(pName))
+                mRegister.Add(pName, pInstance);
+        }
+
+        public static object Lookup(string pName)
+        {
+            if (!mRegister.ContainsKey(pName))
+                Register(pName, (object)Activator.CreateInstance(Type.GetType(pName)));
+            return (object)mRegister[pName];
+        }
+    }
+}
