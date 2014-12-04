3657d50bf25b954cb87a4c418b4d3c14d85468b5
chris <fermat.little.theorem@gmail.com>
2014-12-04 23:05:12 +0800

    Re-construct Synchronized Singleton case.

---
+++ SingletonPattern/Java/SynchronizedSingleton/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String args[])
+    {
+        SSingleton instance1 = SSingleton.getInstance(); 
+        SSingleton instance2 = SSingleton.getInstance(); 
+
+        if(instance1 == instance2)
+        {
+            System.out.println("They're the same instance");
+        }
+    }
+}
---
+++ SingletonPattern/Java/SynchronizedSingleton/SSingleton.java
@@ -0,0 +1,16 @@
+public class SSingleton 
+{
+    private static SSingleton instance = null;
+    
+    private SSingleton() {}
+
+    // Make sure that only one thread can access the instance
+    synchronized static public SSingleton getInstance() 
+    {
+        if (instance == null) 
+        {
+            instance = new SSingleton();
+        }
+        return instance;
+    }
+}
