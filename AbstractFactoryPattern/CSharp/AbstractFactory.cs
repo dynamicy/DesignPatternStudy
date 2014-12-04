a253c1b86b8555a317ee00a7338603b9963b1add
sufrank <su.frank@gmail.com>
2014-12-04 21:40:01 +0800

    Fix abstract factory pattern in C# lose file

---
+++ AbstractFactoryPattern/CSharp/AbstractFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatthenStudy
+{
+    interface AbstractFactory
+    {
+        AbstractProduct_PotatoChip Create_PotatoChip();
+        AbstractProduct_Chocolate Create_Chocolate();
+    }
+}
