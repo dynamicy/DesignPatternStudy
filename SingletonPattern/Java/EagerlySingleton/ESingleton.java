27dc88cb77051083d5860060b1da44ce1c11c685
chris <fermat.little.theorem@gmail.com>
2014-12-04 23:00:59 +0800

    Re-construct Eagerly Singleton case.

---
+++ SingletonPattern/Java/EagerlySingleton/ESingleton.java
@@ -0,0 +1,13 @@
+public class ESingleton
+{
+    // Use static initiallizer, that will make instace created fast.
+    private static ESingleton instance = new ESingleton();
+
+    private ESingleton() {}
+
+    // Just get the instance
+    public static ESingleton getInstance()
+    {
+        return instance;
+    }
+}
---
+++ SingletonPattern/Java/EagerlySingleton/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ESingleton singletonInstance1 = ESingleton.getInstance();
+        ESingleton singletonInstance2 = ESingleton.getInstance();
+
+        if(singletonInstance1 == singletonInstance2)
+        {
+            System.out.println("They're the same instance");
+        }
+    }
+}
