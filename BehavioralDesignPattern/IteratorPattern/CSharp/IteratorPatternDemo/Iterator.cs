924f193c02b0b4dd4cf7c615d5f1da2d44e037f9
sufrank <su.frank@gmail.com>
2015-01-31 21:01:05 +0800

    Add an example of Iterator Pattern in C#

---
+++ BehavioralDesignPattern/IteratorPattern/CSharp/IteratorPatternDemo/Iterator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorPattern
+{
+    public abstract class Iterator
+    {
+        public abstract object First();
+        public abstract object Next();
+        public abstract bool IsDone();
+        public abstract object CurrentItem();
+    }
+}
