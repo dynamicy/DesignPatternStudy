f06cc29b3e6b920d39f09c75b98afd036e535f0f
chrish <chrish@yxunistar.com>
2017-10-23 18:18:06 +0800

    Refactoring ObserverPattern.

--- Behavioral/ObserverPattern/src/io/csie/chris/Main.java
+++ Behavioral/ObserverPattern/src/io/csie/chris/Main.java
@@ -1,20 +1,12 @@
 package io.csie.chris;
 
-import java.util.Observable;
-import java.util.Observer;
-
 public class Main {
 
     public static void main(String args[]) {
 
         EventSource eventSource = new EventSource();
 
-        eventSource.addObserver(new Observer() {
-            @Override
-            public void update(Observable o, Object arg) {
-                System.out.println(arg);
-            }
-        });
+        eventSource.addObserver((o, arg) -> System.out.println(arg));
 
         new Thread(eventSource).start();
     }
