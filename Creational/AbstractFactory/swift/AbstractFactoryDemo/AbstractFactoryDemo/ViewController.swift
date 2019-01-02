605f7c4f3aaf119a9eab0ea1eddb8f08898a3111
Chris <chrissw.huang@optoma.com>
2019-01-02 15:15:28 +0800

    First commit.

---
+++ Creational/AbstractFactory/swift/AbstractFactoryDemo/AbstractFactoryDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AbstractFactoryDemo
+//
+//  Created by Chris Huang 黃信文 (奧圖碼) on 2019/1/2.
+//  Copyright © 2019 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
